3109d0c51c8141f16c350063a8126707827d3c35
tarunspartan <tarunspartan1234@outlook.com>
2020-04-14 00:37:59 +0530

    added player in each array block

--- src/components/Board.js
+++ src/components/Board.js
@@ -9,36 +9,33 @@ const Board = () => {
     const [ Player, setPlayer ] = useState('red')
 
     useEffect(() => {
-        start()
-    },[])
-
-    const start = () => {
         let columns = ~~(window.innerWidth/50)
         setBoardColumns(columns)
         let rows = ~~(window.innerHeight/50)
         setBoardRows(rows)
         let a = new Array(rows).fill(0)
-        let b = a.map(i => a[i] = new Array(columns).fill(0))
+        let b = a.map(i => a[i] = new Array(columns).fill([0,'red']))
         setBoardArray(b)
-    }
+    },[])
 
-    const blockClickHandler = (x,y,d) => {
-        if(d === 'user'){
+    const blockClickHandler = (x,y,Player) => {
+        setTimeout(() => {
+            if(Player)
             changeUser()
-        }
-        // console.log(x,y)
-        // console.log('BoardRows',BoardRows-1)
-        // console.log('BoardColumns',BoardColumns-1)
-        if((x === 0 && y === 0) || (x === 0 && y === BoardColumns-1) || (y === 0 && x === BoardRows-1) || (x === BoardRows-1 && y === BoardColumns-1)){
-            // console.log('Corner')
-            return BoardArray[x][y] === 1 ? splitHandler(x,y,'corner') : incrementHandler(x,y)
-        }else if(x === 0 || y === 0 || x === BoardRows-1 || y === BoardColumns-1){
-            // console.log('Side')
-            return BoardArray[x][y] === 2 ? splitHandler(x,y,'side') : incrementHandler(x,y)
-        }else{  
-            // console.log('Center')
-            return BoardArray[x][y] === 3 ? splitHandler(x,y,'center') : incrementHandler(x,y)
-        }
+            // console.log(x,y)
+            // console.log('BoardRows',BoardRows-1)
+            // console.log('BoardColumns',BoardColumns-1)
+            if((x === 0 && y === 0) || (x === 0 && y === BoardColumns-1) || (y === 0 && x === BoardRows-1) || (x === BoardRows-1 && y === BoardColumns-1)){
+                // console.log('Corner')
+                return BoardArray[x][y][0] === 1 ? splitHandler(x,y,'corner') : incrementHandler(x,y)
+            }else if(x === 0 || y === 0 || x === BoardRows-1 || y === BoardColumns-1){
+                // console.log('Side')
+                return BoardArray[x][y][0] === 2 ? splitHandler(x,y,'side') : incrementHandler(x,y)
+            }else{  
+                // console.log('Center')
+                return BoardArray[x][y][0] === 3 ? splitHandler(x,y,'center') : incrementHandler(x,y)
+            }
+        }, 100); 
     }
 
     const changeUser = () => {
@@ -47,12 +44,15 @@ const Board = () => {
 
     const incrementHandler = (x,y) => {
         // console.log('Increment Handler')
-        BoardArray[x][y] += 1
-        setBoardArray([...BoardArray])
+        let items = [...BoardArray]
+        let item = [...items[x][y]]
+        item[0] += 1
+        items[x][y] = item
+        setBoardArray(items)
     }
 
     const setToZeroHandler = (x,y) => {
-        BoardArray[x][y] = 0
+        BoardArray[x][y][0] = 0
         setBoardArray([...BoardArray])
     }
 
@@ -113,9 +113,15 @@ const Board = () => {
                     return <div key={rowindex} style={{height:'50px'}}>
                     {row.map((col,colindex) => {
                         return <React.Fragment key={rowindex+colindex}>
-                            <div className='block' style={Player === 'red' ? {border:'1px solid red'} : {border:'1px solid green'}}
-                                onClick={() => blockClickHandler(rowindex,colindex,'user')}>
-                                <div className={BoardArray[rowindex][colindex] === 1 ? 'one common' : BoardArray[rowindex][colindex] === 2 ? 'two common' : BoardArray[rowindex][colindex] === 3 ? 'three common' : 'empty'}>
+                            <div className='block' 
+                            // style={Player === 'red' ? {border:'1px solid red'} : {border:'1px solid green'}}
+                                style={{border:`1px solid ${Player}`}}
+                                onClick={() => blockClickHandler(rowindex,colindex,Player)}>
+                                <div className={BoardArray[rowindex][colindex][0] === 1 ? 
+                                                    'one common' : 
+                                                BoardArray[rowindex][colindex][0] === 2 ? 
+                                                    'two common' : BoardArray[rowindex][colindex][0] === 3 ?
+                                                    'three common' : 'empty'}>
 
                                 </div>
                             </div>
