20cace28d5bfc8810dd746de907145d63e9ed972
tarunspartan <tarunspartan1234@outlook.com>
2020-04-19 01:15:17 +0530

    removed install

--- src/components/Board.js
+++ src/components/Board.js
@@ -17,36 +17,6 @@ const Board = () => {
     const [ soundStatus, setSoundStatus ] = useState(localStorage.getItem('sound') || 'on')
 
     useEffect(() => {
-        let deferredPrompt;
-
-        window.addEventListener('beforeinstallprompt', (e) => {
-        // Prevent the mini-infobar from appearing on mobile
-        e.preventDefault();
-        // Stash the event so it can be triggered later.
-        deferredPrompt = e;
-        // Update UI notify the user they can install the PWA
-        // showInstallPromotion();
-        });
-
-        let buttonInstall = document.getElementById('buttonInstall')
-        buttonInstall.addEventListener('click', (e) => {
-            // Hide the app provided install promotion
-            // hideMyInstallPromotion();
-            // Show the install prompt
-            deferredPrompt.prompt();
-            // Wait for the user to respond to the prompt
-            deferredPrompt.userChoice.then((choiceResult) => {
-              if (choiceResult.outcome === 'accepted') {
-                console.log('User accepted the install prompt');
-              } else {
-                console.log('User dismissed the install prompt');
-              }
-            })
-          });
-
-    },[])
-
-    useEffect(() => {
         localStorage.setItem('sound',soundStatus)
     },[soundStatus])
 
@@ -120,7 +90,6 @@ const Board = () => {
                 <div onClick={() => soundButtonHandler()}>
                     {<span role='img' aria-label='sound'>{soundStatus && soundStatus === 'on' ? '🔊' : '🔇'}</span>}
                 </div>
-                <button id='buttonInstall'>Install</button>
                 </div>
             </div>
         )
