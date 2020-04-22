624e4daf07f493a1ea90faa3238c10bada475c61
tarunspartan <tarunspartan1234@outlook.com>
2020-04-22 23:59:13 +0530

    [fixes]

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,8 +3,8 @@ import './Board.css'
 import drop from './drop.mp3'
 let clickable = true
 let flag = 0
-const sound = new Audio(drop)
-sound.playbackRate = 1.75
+// const sound = new Audio(drop)
+// sound.playbackRate = 1.75
 window.onbeforeunload = function(e) {
     return "Reloading this page will reset the game";
 };
@@ -58,7 +58,7 @@ const Board = () => {
     }
 
     const playSound = () => {
-        return soundStatus && soundStatus === 'on' ? sound.play() : null
+        return soundStatus && soundStatus === 'on' ? new Audio(drop).play() : null
     }
 
     const checkWinningStats = () => {
@@ -111,7 +111,7 @@ const Board = () => {
                 <div onClick={() => soundButtonHandler()}>
                     {<span role='img' aria-label='sound'>{soundStatus && soundStatus === 'on' ? '🔊' : '🔇'}</span>}
                 </div>
-                <div className='restart' onClick={() => window.location.reload()}>R e s t a r t&nbsp;<span role='img' aria-label="reload">🔃</span></div>
+                <div className='restart' onClick={() => window.location.reload()}>R e s t a r t</div>
                 <div style={{opacity:0.3,fontSize:'10px',textTransform:'none'}}>Designed & Built with <span role='img' aria-label="love">💙</span> by Tarun</div>
                 </div>
             </div>
@@ -216,8 +216,8 @@ const Board = () => {
                 })
             }
         <div className='footer' style={{marginTop:'10px'}}>
-            <div style={{display:'flex'}}>
-            <div style={{color:'skyblue'}}>S T A Y H O M E - S T A Y S A F E</div>
+            <div style={{display:'flex',alignItems:'center'}}>
+            <div style={{color:'skyblue'}} id="shsf">∘ C H A I N • R E A C T I O N ∘</div>
             <div style={{position:'absolute',right:'5px',fontSize:'20px',cursor:'pointer'}} onClick={() => settingsHandler()}>&nbsp;<span role='img' aria-label="settings"><b>⚙</b></span>&nbsp;</div>
         </div>
         </div>
