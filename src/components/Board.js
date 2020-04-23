2aafe971d0edabb711b737405b28656e2d9ba75d
tarunspartan <tarunspartan1234@outlook.com>
2020-04-23 23:20:23 +0530

    removed PWA

--- src/components/Board.js
+++ src/components/Board.js
@@ -28,38 +28,6 @@ const Board = () => {
         setBoardArray(b)
     },[BoardRows,BoardColumns])
 
-    useEffect(() => {
-        console.log('called')
-        window.addEventListener('beforeinstallprompt', (event) => {
-            console.log('fghj')
-            console.log('👍', 'beforeinstallprompt', event);
-            // Stash the event so it can be triggered later.
-            window.deferredPrompt = event;
-            // Remove the 'hidden' class from the install button container
-            // divInstall.classList.toggle('hidden', false);
-          });
-            let butInstall = document.getElementById('butInstall')
-            butInstall.addEventListener('click', () => {
-                console.log('👍', 'butInstall-clicked');
-                const promptEvent = window.deferredPrompt;
-                if (!promptEvent) {
-                  // The deferred prompt isn't available.
-                  return;
-                }
-                // Show the install prompt.
-                promptEvent.prompt();
-                // Log the result
-                promptEvent.userChoice.then((result) => {
-                  console.log('👍', 'userChoice', result);
-                  // Reset the deferred prompt variable, since
-                  // prompt() can only be called once.
-                  window.deferredPrompt = null;
-                  // Hide the install button.
-                //   divInstall.classList.toggle('hidden', true);
-                });
-              });
-    },[])
-
     const blockClickHandler = (x,y,Player) => {
         flag+=1
         if(Player){
@@ -260,7 +228,6 @@ const Board = () => {
         <div style={{marginTop:'10px'}}>
             <div style={{display:'flex',alignItems:'center'}}>
             <div id="footer">C H A I N <span id="dot">•</span> R E A C T I O N</div>
-            <button id="butInstall">Install</button>
             <div id="settingsIcon" onClick={() => settingsHandler()}>&#x2699;</div>
         </div>
         </div>
