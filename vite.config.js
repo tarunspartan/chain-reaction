0286336106044a23591081d4b94aa6dc3794a5c6
Tarun M <tarunspartan1234@outlook.com>
2026-06-11 14:29:55 +0530

    added files

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/chain-reaction/',
+  base: '',
   plugins: [
     react(),
     VitePWA({
