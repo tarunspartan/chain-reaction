ad4c319aeab57664e13c3f9ed091e63b4220e1f0
tarunspartan <tarunspartan@users.noreply.github.com>
2026-08-15 10:43:09 +0000

    deploy: 755305b092f9300ccb7df04d6679c4c41fe76239

---
+++ preview/pr-40/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/chain-reaction/preview/pr-40/sw.js', { scope: '/chain-reaction/preview/pr-40/' })})}
